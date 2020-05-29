f7c2945a084368c10c8088d1cab0da4a210dd653
jordanswp <sohweepin@gmail.com>
2020-05-29 10:20:39 +0800

    MVP

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import tweets from 'tweets'
+import tweets from 'tweets';
+
 
 class App extends React.Component {
   render() {
+
+    console.log(tweets.tweets);
+
+    let tweet = tweets.tweets;
+
+    let tweetElement = tweet.map((tweet) => {
+        return (
+            <div>
+                <div>{tweet.user.name} @{tweet.user.screen_name}</div>
+                <div>{tweet.text} {tweet.user.entities.url.urls[0].display_url}</div>
+                <p></p>
+            </div>
+            )
+    })
+
+    console.log(tweet);
+    // console.log(tweets.tweets[0]);
+
+    // console.log(tweetStuff[0]);
+
+
     return (
       <div>
-        tweets
+      {tweetElement}
       </div>
     );
   }
@@ -15,6 +37,8 @@ class App extends React.Component {
 
 const element = document.getElementById('app');
 
-ReactDOM.render(<App />, element );//
+ReactDOM.render(
+    <App />,
+    element
+    );//
 
-console.log("tweet react");
